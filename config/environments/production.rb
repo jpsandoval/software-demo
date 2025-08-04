023aebe8e50b1a720e6ff45d8f5f3a9393bddc33
jpsandoval <juampiboy@gmail.com>
2025-08-04 18:16:10 -0400

    adding

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,9 @@ Rails.application.configure do
   # Only use :id for inspections in production.
   config.active_record.attributes_for_inspect = [ :id ]
 
+  config.action_cable.mount_path = nil
+  config.action_cable.url = nil
+  config.action_cable.allowed_request_origins = []
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
