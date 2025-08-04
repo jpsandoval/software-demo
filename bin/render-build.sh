d934bcf3c582d84623ed73a39fd3a86b2f5cadb3
jpsandoval <juampiboy@gmail.com>
2025-08-04 17:35:08 -0400

    adding log

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -7,6 +7,8 @@ bundle install
 bin/rails assets:precompile
 bin/rails assets:clean
 
+echo "DATABASE_URL=$DATABASE_URL"
+
 # If you have a paid instance type, we recommend moving
 # database migrations like this one from the build command
 # to the pre-deploy command:
