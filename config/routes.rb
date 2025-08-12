159c78b4758b54b60b5613d4bf62b7154a3748e8
jpsandoval <juampiboy@gmail.com>
2025-08-11 21:21:18 -0400

    fixing

--- config/routes.rb
+++ config/routes.rb
@@ -10,11 +10,16 @@ Rails.application.routes.draw do
   root "homepage#index" 
   
   
-  get 'hi', to:'hello#sayHi'
+   # 1) Create ticket
   post   "/tickets", to: "tickets#create"
-  get    "/tickets/:code/status", to: "tickets#status"
-  patch   "/tickets/:code/pay", to: “tickets#pay"
+  # 2) Pay ticket (by code)
+  # 9c3bbcaa63
+  patch   "/tickets/:code/pay", to: "tickets#pay"
+  # 3) Check-in
+  patch "/tickets/:code/checkin", to: "tickets#checkin"
+  # 4) Purge unpaid tickets older than 2 days
   delete "/tickets/purge_unpaid", to: "tickets#purge_unpaid"
 
-
+  # 5) Get ticket status by code
+  get    "/tickets/:code/status", to: "tickets#status"
 end
